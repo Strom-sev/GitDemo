5294bd4e035aff8b31b1720832d091d683930b8e
Storm-sev <zq329051@outlook.com>
2017-05-17 16:45:43 +0800

    first commit

---
+++ app/src/main/java/com/storm/gitdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.storm.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+    }
+}
